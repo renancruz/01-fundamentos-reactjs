6a7c391bac90f15b2a91ff4fee86e2191ca3a211
Renan Cruz <renan.frcr@gmail.com>
2023-03-04 16:51:40 -0300

    Utilizando conceito de estado no react

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,19 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 
-
 export function Post({ author, publishedAt, content}) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+  
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -16,6 +22,13 @@ export function Post({ author, publishedAt, content}) {
     locale: ptBR,
     addSuffix: true
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+
+  }
 
   return (
     <article className={styles.post}>
@@ -43,7 +56,7 @@ export function Post({ author, publishedAt, content}) {
         })}
       </div>
 
-      <form className={styles.comentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea 
           placeholder="deixe um comentário"
@@ -55,9 +68,11 @@ export function Post({ author, publishedAt, content}) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return (
+            <Comment/>
+          )
+        })}
       </div>
     </article>
   )
