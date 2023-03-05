ff3676bb2dbb5834ad5f26a0f118c5a184eb383d
Renan Cruz <renan.frcr@gmail.com>
2023-03-05 12:21:59 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({content}) {
+export function Comment({content, onDeleteComment}) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://xesque.rocketseat.dev/users/avatar/profile-7a1804b9-dafc-423b-9266-04509cd8b2d5-1668954972749.jpg" alt="" />
@@ -14,7 +19,7 @@ export function Comment({content}) {
               <strong>Renan Cruz</strong>
               <time title="11 de maio as 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24}/>
             </button>
           </header>
